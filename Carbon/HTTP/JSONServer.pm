64223f5a470cd0c82473351fec083ad7afab4887
mirror <mirror12k@gmail.com>
2019-09-18 13:12:14 -0400

    - implemented a helper class for creating json applications with carbon2

---
+++ Carbon/HTTP/JSONServer.pm
@@ -0,0 +1,34 @@
+package Carbon::HTTP::JSONServer;
+use parent 'Carbon::Router';
+use strict;
+use warnings;
+
+use feature 'say';
+
+use Carp;
+use JSON;
+
+use Carbon::HTTP::Response;
+
+
+
+sub route_json {
+	my ($self, $path, $callback, %opts) = @_;
+
+	return $self->route(qr/$path.*/ => sub {
+		my ($self, $req) = @_;
+		say "debug content: ", $req->content;
+		my $json_res = $callback->($self, decode_json($req->content), $req);
+
+		my $res = Carbon::HTTP::Response->new('200', 'OK');
+		$res->content(encode_json($json_res));
+		$res->header('content-length' => length $res->content);
+		$res->header('content-type' => 'application/json');
+
+		return $res;
+	}, %opts);
+}
+
+
+
+1;
